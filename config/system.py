52b12d2c56163671b0af21f5c30b89ac111b6309
Neerav Karani <nkarani@ethz.ch>
2019-12-06 19:16:16 +0100

    Add files via upload

---
+++ config/system.py
@@ -0,0 +1,39 @@
+import os
+import logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
+
+# ==================================================================
+# SET THESE PATHS MANUALLY #########################################
+# ==================================================================
+
+# ==================================================================
+# name of the host - used to check if running on cluster or not
+# ==================================================================
+local_hostnames = ['bmicdl05']
+
+# ==================================================================
+# project dirs
+# ==================================================================
+project_root = '/usr/bmicnas01/data-biwi-01/nkarani/projects/generative_segmentation/'
+bmic_data_root = '/usr/bmicnas01/data-biwi-01/bmicdatasets-originals/Originals/'
+project_code_root = os.path.join(project_root, 'code/')
+project_data_root = os.path.join(project_root, 'data/')
+
+# ==================================================================
+# data dirs
+# ==================================================================
+orig_data_root_hcp = os.path.join(bmic_data_root,'HCP/3T_Structurals_Preprocessed/')
+orig_data_root_abide = '/usr/bmicnas01/data-biwi-01/nkarani/projects/generative_segmentation/data/preproc_data/abide/'
+orig_data_root_pfizer = '/usr/bmicnas01/data-biwi-01/bmicdatasets-originals/Originals/PfizerData/'
+
+# ==================================================================
+# dirs where the pre-processed data is stored
+# ==================================================================
+preproc_folder_hcp = os.path.join(project_data_root,'preproc_data/hcp/')
+preproc_folder_abide = os.path.join(project_data_root,'preproc_data/abide/')
+preproc_folder_pfizer = os.path.join(project_data_root,'preproc_data/pfizer/')
+
+# ==================================================================
+# log root
+# ==================================================================
+log_root = os.path.join(project_code_root, 'brain/v10/logdir/')
