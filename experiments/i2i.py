138548a7d14b7ffec9b01ce54f5a4837b09f841e
Neerav Karani <nkarani@ethz.ch>
2020-06-18 11:51:17 +0200

    Update i2i.py
    code used for submission

--- experiments/i2i.py
+++ experiments/i2i.py
@@ -6,7 +6,22 @@ import tensorflow as tf
 # ======================================================================
 train_dataset = 'HCPT1' # STANFORD / CALTECH / HCPT2 / 'HCPT1'
 test_dataset = 'CALTECH' # STANFORD / CALTECH / HCPT2
-normalize = False
+normalize = True
+run_num = 1
+dae_atlas_ratio_threshold = 0.0
+min_atlas_dice = 0.0
+use_gt_for_tta = False
+
+# ====================================================
+# normalizer architecture
+# ====================================================
+model_handle_normalizer = model_zoo.net2D_i2i
+norm_kernel_size = 3
+norm_num_hidden_layers = 2
+norm_num_filters_per_layer = 16
+norm_activation = 'rbf'
+norm_batch_norm = False
+i2i_arch_str = 'i2i_' + str(norm_num_hidden_layers) + '_' + str(norm_num_filters_per_layer) + '_k' + str(norm_kernel_size) + '_' + norm_activation + '_bn' + str(int(norm_batch_norm)) + '/'
 
 # ====================================================
 # settings of the image to label mapper (a model for the task)
@@ -15,18 +30,8 @@ model_handle_i2l = model_zoo.unet2D_i2l
 tr_str = 'tr' + train_dataset
 da_ratio = 0.25
 da_str = '_da' + str(da_ratio)
-run_str = '_run' + str(1)
-expname_i2l = tr_str + da_str + run_str
-
-# ====================================================
-# settings of the label to image mapper (likelihood)
-# ====================================================
-model_handle_l2i = model_zoo.unet2D_l2i
-tr_str = 'tr' + train_dataset
-loss_str = '_loss' + 'l2'
-da_str = '_da' + str(0.25)
-run_str = '_run' + str(1)
-expname_l2i = tr_str + loss_str + da_str + run_str
+run_str = '_run' + str(run_num)
+expname_i2l = i2i_arch_str + tr_str + da_str + run_str
 
 # ====================================================
 # settings of the label to label mapper (prior)
@@ -47,39 +52,20 @@ mask_type = 'squares_jigsaw'
 mask_radius = 10 # The mask will be a square with side length twice this number 
 num_squares = 200
 mask_str = '_mask_' + mask_type + '_maxlen' + str(2*mask_radius) + 'x' + str(num_squares)
-run_str = '_run' + str(1)
+run_str = '_run' + str(run_num)
 expname_l2l = tr_str + da_str + res_str + model_str + mask_str + run_str
 
 # ====================================================
-# normalizer architecture
+# normalizer dir name
 # ====================================================
-model_handle_normalizer = model_zoo.net2D_i2i
-norm_kernel_size = 3
-norm_num_hidden_layers = 2
-norm_num_filters_per_layer = 16
-norm_activation = 'rbf'
-learning_rate = 1e-3
-norm_batch_norm = False
-arch_str = str(norm_num_hidden_layers) + '_' + str(norm_num_filters_per_layer) + '_k' + str(norm_kernel_size) + '_' + norm_activation + '_lr' + str(learning_rate) + '_bn' + str(int(norm_batch_norm)) + '/'
-
-# ====================================================
-# weights for the two losses
-# ====================================================
-loss_type_prior = 'dice'  # crossentropy/dice
-lambda_prior = 1.0
-num_prior_samples = 1
-loss_type_likelihood = 'ssim' # l2/ssim
-lambda_likelihood = 0.1
-cae_atlas_ratio_threshold = 0.0
-min_atlas_dice = 0.0
-threshold_str = 'cae_atlas_ratio_' + str(cae_atlas_ratio_threshold) + 'min_atlas_' + str(min_atlas_dice)
-loss_str = '_lossP_' + loss_type_prior + '_' + str(lambda_prior) + threshold_str + '_lossL_' + loss_type_likelihood + '_' + str(lambda_likelihood)
-
 expname_normalizer = 'normalizer/test' + test_dataset 
-expname_normalizer = expname_normalizer + '/i2l_' + expname_i2l + '/l2i_' + expname_l2i + '/l2l_' + expname_l2l
+expname_normalizer = expname_normalizer + '/i2l_' + expname_i2l + '/l2l_' + expname_l2l
+loss_type_prior = 'dice'  # crossentropy/dice
+learning_rate = 1e-3
+loss_str = '/lossP_' + loss_type_prior + '_lr' + str(learning_rate)
 run_number = 1
 run_str = '/run' + str(run_number)
-expname_normalizer = expname_normalizer + '/norm_' + arch_str + loss_str + run_str
+expname_normalizer = expname_normalizer + loss_str + run_str
 
 # ======================================================================
 # data settings
@@ -100,8 +86,8 @@ batch_size_downsampled = int(batch_size / downsampling_factor_x)
 # training settings
 # ======================================================================
 continue_run = True
-max_steps = int(image_depth_hcp / batch_size)*600 + 1
+max_steps = int(image_depth_hcp / batch_size)*100 + 1
 optimizer_handle = tf.train.AdamOptimizer
 debug = False
 check_ood_frequency = int(image_depth_hcp / batch_size)*25
-vis_frequency = int(image_depth_hcp / batch_size)*75 # ensure that this is a multiple of "check_ood_frequency"
+vis_frequency = int(image_depth_hcp / batch_size)*100 # ensure that this is a multiple of "check_ood_frequency"
