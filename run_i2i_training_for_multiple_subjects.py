2ca537e86875b0826aad8fac25774df4756bcb2d
Neerav Karani <nkarani@ethz.ch>
2020-06-18 12:03:18 +0200

    Delete run_i2i_training_for_multiple_subjects.py

--- run_i2i_training_for_multiple_subjects.py
+++
@@ -1,4 +0,0 @@
-import subprocess
-
-for subject_id in range(20):
-    subprocess.call(['python', '/usr/bmicnas01/data-biwi-01/nkarani/projects/generative_segmentation/code/brain/v2.0/update_i2i_mapper.py', str(subject_id)])
